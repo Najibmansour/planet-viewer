04b57295725865fc2f583c78479b8a82a930cf2d
Najib Mansour BitBucket <najib.mansour@axentech.io>
2025-06-30 04:50:33 +0300

    fix left sides

--- app/planets/[slug]/components/left/SideLeft.tsx
+++ app/planets/[slug]/components/left/SideLeft.tsx
@@ -9,22 +9,22 @@ const SideLeft = ({ planet }: Props) => {
   return (
     <div className="z-50 col-span-1 col-start-1 row-span-4 row-start-2 mt-24 flex p-16">
       <div className="space-y-7 text-2xl leading-7">
-        <h3 className="text-nowrap text-gray-400/5 transition-all hover:text-3xl hover:text-white">
+        <h3 className="text-nowrap text-white/50 transition-all hover:text-3xl hover:text-white">
           {planet.description}
         </h3>
-        <h3 className="text-nowrap text-gray-400/5 transition-all hover:text-3xl hover:text-white">
+        <h3 className="text-nowrap text-white/50 transition-all hover:text-3xl hover:text-white">
           <span className="font-bold tracking-wider">Orbital Period: </span>
           {planet.orbitalPeriod}
         </h3>
-        <h3 className="text-nowrap text-gray-400/5 transition-all hover:text-3xl hover:text-white">
+        <h3 className="text-nowrap text-white/50 transition-all hover:text-3xl hover:text-white">
           <span className="font-bold tracking-wider">Gravity:</span>{" "}
           {planet.gravity}
         </h3>
-        <h3 className="text-nowrap text-gray-400/5 transition-all hover:text-3xl hover:text-white">
+        <h3 className="text-nowrap text-white/50 transition-all hover:text-3xl hover:text-white">
           <span className="font-bold tracking-wider">Radius:</span>{" "}
           {planet.radius}
         </h3>
-        <h3 className="text-nowrap text-gray-400/5 transition-all hover:text-3xl hover:text-white">
+        <h3 className="text-nowrap text-white/50 transition-all hover:text-3xl hover:text-white">
           <span className="font-bold tracking-wider">Distance From Sun: </span>
           {planet.distanceFromSun}
         </h3>
